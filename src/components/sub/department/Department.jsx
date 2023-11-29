28b2256454357d5a5c87264af4d23c3462690089
seohyon-cho <flanique@naver.com>
2023-11-29 10:11:50 +0900

    복잡한 구조의 json파일을 가상돔으로 출력

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -8,6 +8,9 @@ export default function Department() {
 	const shortTitle = useCustomText('short');
 	const [MemberTit, setMemberTit] = useState('');
 	const [MemberData, setMemberData] = useState([]);
+	const [HistoryTit, setHistoryTit] = useState('');
+	const [HistoryData, setHistoryData] = useState([]);
+
 	const path = useRef(process.env.PUBLIC_URL);
 
 	const test1 = 'our+members-score_abc';
@@ -21,13 +24,42 @@ export default function Department() {
 				setMemberData(Object.values(json)[0]);
 			});
 	};
+	const fetchHistory = () => {
+		fetch(`${path.current}/DB/history.json`)
+			.then((data) => data.json())
+			.then((json) => {
+				setHistoryTit(Object.keys(json)[0]);
+				setHistoryData(Object.values(json)[0]);
+			});
+	};
 
 	useEffect(() => {
 		fetchDepartment();
+		fetchHistory();
 	}, []);
 
 	return (
 		<Layout2 title={'Department'}>
+			<section className='historyBox'>
+				<h2>{combinedTitle(HistoryTit)}</h2>
+				<div className='con'>
+					{/* HistoryData가 반복도는 각각의 데이터 {년도: 배열} */}
+					{HistoryData.map((history, idx) => {
+						return (
+							<article key={history + idx}>
+								{/* 현재 반복돌고 있는 개체의 key값을 뽑아서 h3로 출력 :2016, 2018, 2020 */}
+								<h3>{Object.keys(history)[0]}</h3>
+								<ul>
+									{/* 현재 반복돌고 있는 개체의 value값을 뽑아서, li로 반복 출력 [문자열, 문자열] */}
+									{Object.values(history)[0].map((list, idx) => {
+										return <li key={list + idx}>{list}</li>;
+									})}
+								</ul>
+							</article>
+						);
+					})}
+				</div>
+			</section>
 			<section className='memberBox'>
 				<h2>{combinedTitle(MemberTit)}</h2>
 				<div className='con'>
@@ -47,30 +79,3 @@ export default function Department() {
 		</Layout2>
 	);
 }
-
-/*
-	React에서 외부 데이터를 가져와서, 화면에 동적으로 출력하는 순서 
-
-	1. 외부 데이터를 가져와서 담을 빈 state 추가 (보통은 빈 배열로 초기화 먼저 함.)
-	2. fetch문을 이용해서, 특정 URL의 데이터를 가져온 뒤, 동기적으로 배열로 뽑아서 state에 담아주는 함수를 정의.
-	3. 위에서 만든 함수를 의존성 배열이 비어있는 useEffect문 안쪽에서 호출
-	4. state에 담겨있는 data 배열 값을 map으로 반복돌면서 JSX 구문 생성.
-
-	객체의 property에서, key와 value값을 반복도는 방법
-
-	(예)
-	const student = {name: 'David', age: 20}
-	
-	// key 반복 돌면서 배열 반환
-	Object.keys(student); --> ['name', 'age'];
-	Object.values(student); --> ['David', 20];
-
-	
-	// 문자열 관련 내장 메서드
-	전체문자열.charAt(순서값) : 전체 문자열에서, 해당 순서의 문자값만 반환
-	전체문자열.slice(순서값1, 순서값2) : 전체 문자열에서, 해당 순서1부터 순서2의 위치까지 문자를 잘라서 반환 
-	전체문자열.toUpperCase() : 문자열 전체를 대문자로 반환
-	전체문자열.toLowerCase() : 문자열 전체를 소문자로 반환 
-	전체문자열.split(구분자) : 전체 문자열을, 구분자를 기준으로 나누어 배열로 반환 
-	배열.join('구분자') : 각 배열값을 구분자로 이어붙이면서, 하나의 문자열로 반환 
-*/
