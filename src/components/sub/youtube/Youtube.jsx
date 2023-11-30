ec550d1346fc5b18b39ae76471f32cfa4c832fb0
seohyon-cho <flanique@naver.com>
2023-11-30 11:00:28 +0900

    state 배열값을 반복 돌면서 JSX에 출력하고, 문자열을 커스텀훅으로 가공. (유튜브 컨텐츠 출력 관련)

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,16 +1,14 @@
 import Layout2 from '../../common/layout2/Layout2';
 import './Youtube.scss';
 import { useState, useEffect } from 'react';
+import { useCustomText } from '../../../hooks/useText';
 
 export default function Youtube() {
+	const customText = useCustomText('combined');
+	const shortenText = useCustomText('short');
 	const [Vids, setVids] = useState([]);
 	console.log(Vids);
 
-	/*
-		기존의 promise then 구문을 async await 로 변경하기 위한 조건 2가지 
-		(1) 무조건 promise 반환 함수를 감싸주는 wrapping 함수가 필요함. (wrapping함수에 async 지정)
-		(2) await 문은, promise 반환 함수에만 지정 가능. (fetch 처럼)
-	*/
 	const fetchYoutube = async () => {
 		const api_key = 'AIzaSyDC60bIIkAJFzy7ji4a0Eo3AX6tYudhe1w';
 		const pid = 'PLYOPkdUKSFgWqafuDQN9di3uLJoTV3L3W';
@@ -24,12 +22,6 @@ export default function Youtube() {
 		} catch (err) {
 			console.error(err);
 		}
-
-		// fetch(baseURL)
-		// 	.then((data) => data.json())
-		// 	.then((json) => {
-		// 		setVids(json.items);
-		// 	});
 	};
 
 	useEffect(() => {
@@ -39,7 +31,27 @@ export default function Youtube() {
 	return (
 		<div className='Youtube'>
 			<Layout2 title={'Youtube'}>
-				<p>youtube 전용 콘텐츠</p>
+				{Vids.map((data, idx) => {
+					const [date, time] = data.snippet.publishedAt.split('T');
+
+					return (
+						<article key={data.id}>
+							<h2>{shortenText(data.snippet.title, 50)}</h2>
+
+							<div className='txt'>
+								<p>{shortenText(data.snippet.description, 250)}</p>
+								<div className='infoBox'>
+									<span>{customText(date, '.')}</span>
+									<em>{time.split('Z')[0]}</em>
+								</div>
+							</div>
+
+							<div className='pic'>
+								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+							</div>
+						</article>
+					);
+				})}
 			</Layout2>
 		</div>
 	);
