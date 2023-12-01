241fe8340d64f3b9d5da2a869fc6ca5d93b9b4e8
seohyon-cho <flanique@naver.com>
2023-12-01 11:08:37 +0900

    flickr data 호출

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react';
 import Layout2 from '../../common/layout2/Layout2';
 import './Gallery.scss';
 
 export default function Gallery() {
+	const [Pics, setPics] = useState([]);
+
+	const fetchFlickr = async () => {
+		console.log('flickr');
+		const num = 500;
+		const flickr_api = '9714d0fe77bde97690ff70f0d88f4d40';
+		const method_interest = 'flickr.interestingness.getList';
+		const baseURL = 'https://www.flickr.com/services/rest/?method=';
+		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
+
+		const data = await fetch(resultURL);
+		const json = await data.json();
+
+		setPics(json.photos.photo);
+	};
+
+	useEffect(() => {
+		fetchFlickr();
+	}, []);
+
 	return (
-		<div className='Gallery'>
-			<Layout2 title={'Gallery'}>
-				<p>gallery 전용 콘텐츠</p>
-			</Layout2>
-		</div>
+		<Layout2 title={'Gallery'}>
+			{Pics.map((pic, idx) => {
+				return (
+					<article key={pic.id}>
+						<h2>{pic.title}</h2>
+					</article>
+				);
+			})}
+		</Layout2>
 	);
 }
