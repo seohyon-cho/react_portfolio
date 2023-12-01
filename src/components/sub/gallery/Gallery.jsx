d5888f6e48251e8f1ee62000841b9710d737acaa
seohyon-cho <flanique@naver.com>
2023-12-01 12:56:21 +0900

    사이간격 고정 px값으로 적용해서 배치하는 방법

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -27,37 +27,37 @@ export default function Gallery() {
 
 	return (
 		<Layout2 title={'Gallery'}>
-			<section>
-				<Masonry
+			<section className='frame'>
+				{/* <Masonry
 					className={'Picframe'}
 					elementType={'div'}
 					options={{ transitionDuration: 0.5 }}
 					disableImagesLoaded={false}
 					updateOnEachImageLoad={false}
-				>
-					{Pics.map((pic, idx) => {
-						return (
-							<article key={pic.id}>
-								<div className='pic'>
-									<img
-										src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-										alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-									/>
-								</div>
-								<h2>{pic.title}</h2>
+				> */}
+				{Pics.map((pic, idx) => {
+					return (
+						<article key={pic.id}>
+							<div className='pic'>
+								<img
+									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+									alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+								/>
+							</div>
+							<h2>{pic.title}</h2>
 
-								<div className='profile'>
-									<img
-										src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-										alt='사용자 프로필 이미지'
-										onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
-									/>
-									<span>${pic.owner}</span>
-								</div>
-							</article>
-						);
-					})}
-				</Masonry>
+							<div className='profile'>
+								<img
+									src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+									alt='사용자 프로필 이미지'
+									onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+								/>
+								<span>${pic.owner}</span>
+							</div>
+						</article>
+					);
+				})}
+				{/* </Masonry> */}
 			</section>
 		</Layout2>
 	);
