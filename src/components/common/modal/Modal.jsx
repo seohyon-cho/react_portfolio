4749618549cb51b0c63b6ff9a708f22bd91ff7f3
seohyon-cho <flanique@naver.com>
2023-12-05 13:16:38 +0900

    framer-motion 시간차(delay) 설정

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -22,10 +22,18 @@ export default function Modal({ Open, setOpen, children }) {
 					className='Modal'
 					initial={{ opacity: 0, scale: 0, rotate: -45 }}
 					animate={{ opacity: 1, scale: 1, rotate: 0 }}
-					exit={{ opacity: 0, scale: 2, rotate: 45 }}
-					transition={{ duration: 0.5 }}
+					exit={{ opacity: 0, scale: 2, rotate: 45, transition: { delay: 0.5 } }}
+					transition={{ duration: 1 }}
 				>
-					<div className='con'>{children}</div>
+					<motion.div
+						className='con'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0, transition: { delay: 0 } }}
+						transition={{ duration: 0.5, delay: 1 }}
+					>
+						{children}
+					</motion.div>
 					<span onClick={() => setOpen(false)}>close</span>
 				</motion.aside>
 			)}
