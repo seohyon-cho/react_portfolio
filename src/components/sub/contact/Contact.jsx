f74498e201f4127aa8a73cf28d6318144d1ccb7e
seohyon-cho <flanique@naver.com>
2023-12-08 12:43:15 +0900

    마크업 위치 정밀하게 보정하기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,18 +3,20 @@ import Layout2 from '../../common/layout2/Layout2';
 import './Contact.scss';
 
 export default function Contact() {
-	const mapFrame = useRef(null);
 	const { kakao } = window;
+	const mapFrame = useRef(null);
+
 	const mapOption = useRef({
-		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-		level: 3, // 지도의 확대 레벨
+		// 위치값 정밀하게 보정하는 법
+		// 기존 구글지도 위치값 복사한 뒤, 카카오 지도 api 사이즈에서 '클릭한 위치의 마커 표시 직접해보기'에서 해당 코드를 붙여넣기 하고, 원하는 지점을 찍으면 하단에 정밀한 값이 뜸.
+		center: new kakao.maps.LatLng(37.51067820147149, 127.04575409412008),
+		level: 3,
 	});
 
 	useEffect(() => {
 		const mapInstance = new kakao.maps.Map(mapFrame.current, mapOption.current);
-		var posInstance = new kakao.maps.LatLng(33.450701, 126.570667);
-		var markerInstance = new kakao.maps.Marker({
-			position: posInstance,
+		const markerInstance = new kakao.maps.Marker({
+			position: mapOption.current.center,
 		});
 
 		markerInstance.setMap(mapInstance);
