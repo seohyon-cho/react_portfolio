5288b480ca83ddd9abe439f56b81e9eda009c8bd
seohyon-cho <flanique@naver.com>
2023-12-08 10:42:49 +0900

    활성화되어있는 버튼 재클릭 시 재활성화 방지

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -11,7 +11,7 @@ export default function Community() {
 
 	const len = useRef(0); // 전체 Post 갯수를 담을 참조 객체
 	const pageNum = useRef(0); // 전체 페이지 갯수를 추후에 연산해서 담을 참조 객체
-	const perNum = useRef(4); // 한 페이지당 보일 Post 갯수
+	const perNum = useRef(6); // 한 페이지당 보일 Post 갯수
 
 	const changeText = useCustomText('combined');
 
@@ -122,7 +122,7 @@ export default function Community() {
 						.fill()
 						.map((_, idx) => {
 							return (
-								<button key={idx} onClick={() => setCurNum(idx)} className={idx === CurNum ? 'on' : ''}>
+								<button key={idx} onClick={() => idx !== CurNum && setCurNum(idx)} className={idx === CurNum ? 'on' : ''}>
 									{idx + 1}
 								</button>
 							);
