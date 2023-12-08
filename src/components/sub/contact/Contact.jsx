cc6419bd4e19ea6d325ce04e3125021a16346ebc
seohyon-cho <flanique@naver.com>
2023-12-08 11:38:46 +0900

    지도 생성 (내용 안 뜨는 오류)

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useRef } from 'react';
 import Layout2 from '../../common/layout2/Layout2';
 import './Contact.scss';
 
 export default function Contact() {
+	const mapFrame = useRef(null);
+	const { kakao } = window;
+	const mapOption = useRef({
+		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level: 3, // 지도의 확대 레벨
+	});
+
+	useEffect(() => {
+		var map = new kakao.maps.Map(mapFrame.current, mapOption.current);
+	}, []);
+
 	return (
 		<div className='Contact'>
 			<Layout2 title={'Contact'}>
-				<p>contact 전용 콘텐츠</p>
+				<article id='map' ref={mapFrame}></article>
 			</Layout2>
 		</div>
 	);
