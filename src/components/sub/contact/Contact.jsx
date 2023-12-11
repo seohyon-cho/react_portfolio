cc0c79dc5f22af97e1a40766080ddf06c0b1bc51
seohyon-cho <flanique@naver.com>
2023-12-11 10:48:36 +0900

    교통량 정보 토글기능 추가 (state 활용)

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -10,6 +10,8 @@ export default function Contact() {
 	const mapFrame = useRef(null);
 	const marker = useRef(null);
 	const mapInstance = useRef(null);
+	// 교통정보 관련 state
+	const [Traffic, setTraffic] = useState(false);
 
 	const setCenter = () => mapInstance.current.setCenter(mapInfo.current[Index].latlng);
 
@@ -47,23 +49,38 @@ export default function Contact() {
 
 	// 컴포넌트 마운트 시, 참조객체에 담아놓은 돔 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
+		// 지도 중첩 생성 안 되도록 생성 직전 초기화 작업
+		mapFrame.current.innerHTML = '';
 		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, { center: mapInfo.current[Index].latlng, level: 3 });
 		marker.current.setMap(mapInstance.current);
-
+		// Index가 바뀔 때마다 setTraffic이 다시 false로 기본셋팅 되도록.
+		setTraffic(false);
 		window.addEventListener('resize', setCenter);
 		return () => window.removeEventListener('resize', setCenter);
 	}, [Index]);
 
+	// 교통정보 관련 false, true를 담은 state를 만들고, 해당 state의 값에 따라서 특정 값을 출력하는 함수를 만드는 것.
+	useEffect(() => {
+		Traffic
+			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
+			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
+
 	return (
 		<div className='Contact'>
 			<Layout2 title={'Contact'}>
-				<ul className='branch'>
-					{mapInfo.current.map((el, idx) => (
-						<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
-							{el.title}
-						</button>
-					))}
-				</ul>
+				<div className='controlBox'>
+					<nav className='branch'>
+						{mapInfo.current.map((el, idx) => (
+							<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
+								{el.title}
+							</button>
+						))}
+					</nav>
+					<nav className='info'>
+						<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
+					</nav>
+				</div>
 				<article id='map' ref={mapFrame}></article>
 			</Layout2>
 		</div>
