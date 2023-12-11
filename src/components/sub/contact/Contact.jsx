34e8d9ee50b79de479f00dd62bcb9cf927e109ff
seohyon-cho <flanique@naver.com>
2023-12-11 10:10:41 +0900

    카카오 객체 참조객체로 옮겨서 의존성 배열에서 kakao 제거 (나중에 useCallback, useMemo 라는 번거로운 걸 안하기 위해서 미리 작업하는 것)

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,51 +3,61 @@ import Layout2 from '../../common/layout2/Layout2';
 import './Contact.scss';
 
 export default function Contact() {
-	const [Index, setIndex] = useState(1);
-
-	const { kakao } = window;
+	// const { kakao } = window;
+	const kakao = useRef(window.kakao);
+	// 화면에 출력될 지도 정보 배열의 순번이 담길 state
+	const [Index, setIndex] = useState(0);
 	const mapFrame = useRef(null);
+	const marker = useRef(null);
+
 	// 참조객체를 사용해, 지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
+	// 지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
 	const mapInfo = useRef([
 		{
 			title: '삼성역 코엑스',
-			latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+			latlng: new kakao.current.maps.LatLng(37.51100661425726, 127.06162026853143),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
 		},
 		{
 			title: '넥슨 본사',
-			latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+			latlng: new kakao.current.maps.LatLng(37.40211707077346, 127.10344953763003),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
 		},
 		{
 			title: '서울 시청',
-			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+			latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
 		},
 	]);
 
 	// 마커 인스턴스 생성
-	const markerInstance = new kakao.maps.Marker({
+	marker.current = new kakao.current.maps.Marker({
 		position: mapInfo.current[Index].latlng,
-		image: new kakao.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt),
+		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt),
 	});
 
+	// 컴포넌트 마운트 시, 참조객체에 담아놓은 돔 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
-		const mapInstance = new kakao.maps.Map(mapFrame.current, { center: mapInfo.current[Index].latlng, level: 3 });
-		markerInstance.setMap(mapInstance);
-
-		// 마커 생성?
-	}, []);
+		const mapInstance = new kakao.current.maps.Map(mapFrame.current, { center: mapInfo.current[Index].latlng, level: 3 });
+		marker.current.setMap(mapInstance);
+	}, [Index]);
 
 	return (
 		<div className='Contact'>
 			<Layout2 title={'Contact'}>
+				<ul className='branch'>
+					{mapInfo.current.map((el, idx) => (
+						<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
+							{el.title}
+						</button>
+					))}
+				</ul>
 				<article id='map' ref={mapFrame}></article>
 			</Layout2>
 		</div>
