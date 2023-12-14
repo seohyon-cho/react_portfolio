bc9c004c39d49f8958b022aadfd5490d6df4df6b
seohyon-cho <flanique@naver.com>
2023-12-14 11:52:31 +0900

    reducer에서 전달받은 데이터를 store 전역 객체에 저장하고, App루트에 컴포넌트로 전달

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
 // 전역 데이터가 관리되는 곳
+import { createStore } from 'redux';
+import reducers from './reducer';
+
+// store 공간을 생성한 다음, reducer가 전달해주는 데이터를 저장하는 것.
+const store = createStore(reducers);
+
+export default store;
