6db7c39f98e48ef1ea7721631615bb36c5dcdbc1
seohyon-cho <flanique@naver.com>
2023-12-15 11:06:12 +0900

    메인 비주얼 컴포넌트에서 전역 유튜브 데이터 미리보기 호출

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,22 @@
+import { useSelector } from 'react-redux';
 import './Visual.scss';
 
 export default function Visual() {
-	return <figure className='Visual'>Visual</figure>;
+	const YoutubeData = useSelector(store => store.youtubeReducer.youtube);
+	return (
+		<figure className='Visual'>
+			<div className='thumbnails'>
+				{YoutubeData?.map((vid, idx) => {
+					if (idx >= 4) return null;
+					return (
+						<article key={vid.id}>
+							<div className='pic'>
+								<img src={vid.snippet.thumbnails.medium.url} alt={vid.snippet.title} />
+							</div>
+						</article>
+					);
+				})}
+			</div>
+		</figure>
+	);
 }
