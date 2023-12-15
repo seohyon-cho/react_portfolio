575024f5f242f472837d9d8bdd10f821b4ce5c37
seohyon-cho <flanique@naver.com>
2023-12-15 11:45:34 +0900

    데이터별 action의 type을 객체로 등록 후 내보내기

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,1 +1,18 @@
 // reducer에 요청을 보내는 함수가 있는 곳
+// 각 데이터 카테고리별 사용될 action의 type명을 변수처럼 모아놓은 객체
+
+export const MEMBER = {
+	start: 'MEMBER_START',
+	success: 'MEMBER_SUCCESS',
+	fail: 'MEMBER_FAIL'
+};
+export const HISTORY = {
+	start: 'HISTORY_START',
+	success: 'HISTORY_SUCCESS',
+	fail: 'HISTORY_FAIL'
+};
+export const YOUTUBE = {
+	start: 'YOUTUBE_START',
+	success: 'YOUTUBE_SUCCESS',
+	fail: 'YOUTUBE_FAIL'
+};
