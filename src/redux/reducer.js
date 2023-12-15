6536557532658ed6f6fe8ca4deac21c690cd991a
seohyon-cho <flanique@naver.com>
2023-12-15 09:45:02 +0900

    fetch함수를 async await버전으로 고도화

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,6 @@
 // store에 요청을 보내는 함수가 있는 곳
 import { combineReducers } from 'redux';
 
-// 순서 (1) - reducer 함수가 호출 되면서, 빈 배열 [] 로, 멤버 데이터가 저장될 state 값 초기화.
-
 const memberReducer = (state = [], action) => {
 	switch (action.type) {
 		case 'SET_MEMBERS':
