982bc98042191a983b8ac75f39769d299e76f093
seohyon-cho <flanique@naver.com>
2023-12-27 14:49:56 +0900

    youtube-detail : 상세페이지 리액트 쿼리 데이터 관리

--- src/hooks/useYoutubeQuery.js
+++ src/hooks/useYoutubeQuery.js
@@ -19,3 +19,21 @@ export const useYoutubeQuery = () => {
 		scaleTime: 1000 * 60 * 60 * 24
 	});
 };
+const fetchYoutubeById = async ({ queryKey }) => {
+	const api_key = process.env.REACT_APP_YOUTUBE_API;
+	const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${queryKey[1]}`;
+
+	const data = await fetch(baseURL);
+	const json = await data.json();
+	return json.items[0].snippet;
+};
+
+export const useYoutubeQueryById = id => {
+	return useQuery(['fetchYoutube', id], fetchYoutubeById, {
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+		cacheTime: 1000 * 60 * 60 * 24,
+		scaleTime: 1000 * 60 * 60 * 24,
+		retry: 3
+	});
+};
