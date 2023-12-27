ec0c7d26648e917b43749e0e0b171e3c0a98cb36
seohyon-cho <flanique@naver.com>
2023-12-27 12:42:11 +0900

    react-query 셋팅 완료

--- src/App.jsx
+++ src/App.jsx
@@ -16,24 +16,30 @@ import { useMedia } from './hooks/useMedia';
 import Menu from './components/common/menu/Menu';
 import Detail from './components/sub/youtube/Detail';
 
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 export default function App() {
 	const [Dark, setDark] = useState(false);
 	const [Toggle, setToggle] = useState(false);
-	const [Open, setOpen] = useState(false);
+	const queryClient = new QueryClient();
 
 	return (
-		<div className={`wrap ${Dark ? 'dark' : ''} ${useMedia()}`}>
-			<Header2 Dark={Dark} setDark={setDark} Toggle={Toggle} setToggle={setToggle} />
-			<Route exact path='/' component={MainWrap} />
-			<Route path='/department' component={Department} />
-			<Route path='/gallery' component={Gallery} />
-			<Route path='/community' component={Community} />
-			<Route path='/members' component={Members} />
-			<Route path='/contact' component={Contact} />
-			<Route path='/youtube' component={Youtube} />
-			<Route path='/detail/:id' component={Detail} />
-			<Footer2 />
-			{Toggle && <Menu setToggle={setToggle} />}
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className={`wrap ${Dark ? 'dark' : ''} ${useMedia()}`}>
+				<Header2 Dark={Dark} setDark={setDark} Toggle={Toggle} setToggle={setToggle} />
+				<Route exact path='/' component={MainWrap} />
+				<Route path='/department' component={Department} />
+				<Route path='/gallery' component={Gallery} />
+				<Route path='/community' component={Community} />
+				<Route path='/members' component={Members} />
+				<Route path='/contact' component={Contact} />
+				<Route path='/youtube' component={Youtube} />
+				<Route path='/detail/:id' component={Detail} />
+				<Footer2 />
+				{Toggle && <Menu setToggle={setToggle} />}
+			</div>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
