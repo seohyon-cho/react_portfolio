58acba3f5a44d48c2198ac9b5a6aff5e43d4053d
seohyon-cho <flanique@naver.com>
2023-12-28 15:49:43 +0900

    다크모드 Dark state 정보값 쿠키에 저장

--- src/components/common/darkMode/DarkMode.jsx
+++ src/components/common/darkMode/DarkMode.jsx
@@ -1,8 +1,17 @@
+import { useCookie } from '../../../hooks/useCookie';
+import { useGlobalData } from '../../../hooks/useGlobalData';
 import './DarkMode.scss';
 
 export default function DarkMode() {
+	const { Dark, setDark } = useGlobalData();
+	const { setCookie } = useCookie();
+	const changeMode = () => {
+		setDark(!Dark);
+		setCookie('dark', !Dark, 20);
+	};
+
 	return (
-		<div className={`DarkMode`}>
+		<div className={`DarkMode ${Dark ? 'dark' : ''}`} onClick={changeMode}>
 			<div className='ball'></div>
 		</div>
 	);
