034cd85001e38fd165753f496c85f15a8a37e1a9
seohyon-cho <flanique@naver.com>
2023-12-29 09:48:43 +0900

    메인페이지 visual 컴포넌트에 유튜브 전역데이터 끌어와서 미리보기로 출력

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,19 @@
 import './Visual.scss';
+import { useSelector } from 'react-redux';
 
 export default function Visual() {
-	return <figure className='Visual'>Visual</figure>;
+	const { youtube } = useSelector(store => store.youtubeReducer);
+	console.log(youtube);
+	return (
+		<figure className='Visual'>
+			{youtube.map((vid, idx) => {
+				if (idx >= 5) return null;
+				return (
+					<article key={vid.id}>
+						<h3>{vid.snippet.title}</h3>
+					</article>
+				);
+			})}
+		</figure>
+	);
 }
