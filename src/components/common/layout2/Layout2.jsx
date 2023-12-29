50df83f2011ac531d9aaaea7b5986e2c61790fff
seohyon-cho <flanique@naver.com>
2023-12-29 17:51:08 +0900

    layout컴포넌트에서 일정시간 이후에 on클래스 적용하는 구문에 옵셔널 체이닝으로 오류방지

--- src/components/common/layout2/Layout2.jsx
+++ src/components/common/layout2/Layout2.jsx
@@ -10,7 +10,7 @@ export default function Layout2({ children, title }) {
 	useEffect(() => {
 		splitText(refTitle.current, title, 0.7, 0.1);
 		setTimeout(() => {
-			refFrame.current.classList.add('on');
+			refFrame.current?.classList.add('on');
 		}, 300);
 	}, [splitText, title]);
 
