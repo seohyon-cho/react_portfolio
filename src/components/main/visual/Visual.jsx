59b6afcd0179d49820f4d36f3e0d638f06987dcf
seohyon-cho <flanique@naver.com>
2023-12-29 10:16:37 +0900

    기본 swiper기능 적용 및 스타일링

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,19 +1,26 @@
 import './Visual.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import { useSelector } from 'react-redux';
 
 export default function Visual() {
 	const { youtube } = useSelector(store => store.youtubeReducer);
-	console.log(youtube);
 	return (
 		<figure className='Visual'>
-			{youtube.map((vid, idx) => {
-				if (idx >= 5) return null;
-				return (
-					<article key={vid.id}>
-						<h3>{vid.snippet.title}</h3>
-					</article>
-				);
-			})}
+			<Swiper>
+				{youtube.map((vid, idx) => {
+					if (idx >= 5) return null;
+					return (
+						<SwiperSlide key={vid.id}>
+							<div className='inner'>
+								<h3>{vid.snippet.title}</h3>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
 		</figure>
 	);
 }
+
+// npm install swiper@8 스와이퍼 설치 명령어
