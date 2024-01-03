af4165f2f7f14abf57d11f258f03e511c2978d7f
seohyon-cho <flanique@naver.com>
2024-01-03 14:46:30 +0900

    swiper 동적 생성

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,32 @@
 import './Visual.scss';
+import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export default function Visual() {
-	return <figure className='Visual'>Visual</figure>;
+	const { isSuccess, data } = useYoutubeQuery();
+	console.log(data);
+
+	return (
+		<figure className='Visual'>
+			<Swiper>
+				{isSuccess &&
+					data.map((el, idx) => {
+						if (idx >= 5) return null;
+						return (
+							<SwiperSlide key={el.id}>
+								<div className='pic'>
+									<p>
+										<img src={el.snippet.thumbnails.standard.url} alt={el.snippet.title} />
+									</p>
+									<p>
+										<img src={el.snippet.thumbnails.standard.url} alt={el.snippet.title} />
+									</p>
+								</div>
+							</SwiperSlide>
+						);
+					})}
+			</Swiper>
+		</figure>
+	);
 }
