ac50bdefc060e947314a237fab999dd64274ca6f
seohyon-cho <flanique@naver.com>
2024-01-03 12:03:34 +0900

    youtube 데이터 main visual에 끌어와서 출력

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
 import './Visual.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function Visual() {
-	return <figure className='Visual'>Visual</figure>;
+	const Vids = useSelector(store => store.youtube.data);
+	return (
+		<figure className='Visual'>
+			<Swiper>
+				{Vids.map((data, idx) => {
+					if (idx >= 5) return null;
+					return (
+						<SwiperSlide key={data.id}>
+							<div className='inner'>
+								<div className='picBox'>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</div>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</figure>
+	);
 }
