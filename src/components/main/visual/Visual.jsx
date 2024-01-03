530eb55b2d02e099a249d0023e5343a825d3286f
seohyon-cho <flanique@naver.com>
2024-01-04 08:05:18 +0900

    현재 활성화 페이지의 순번을 기준으로 좌우(prev, next) 썸네일 미리보기 출력 & 전체 panel 갯수를 참조객체에 등록

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,27 +1,34 @@
 import './Visual.scss';
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
 import 'swiper/css';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 export default function Visual() {
+	const num = useRef(5);
 	const { isSuccess, data } = useYoutubeQuery();
-	const [Index, setIndex] = useState(0);
+	const [Index, setIndex] = useState(1);
+	const [PrevIndex, setPrevIndex] = useState(0);
+	const [NextIndex, setNextIndex] = useState(2);
 
 	const swiperOpt = useRef({
+		modules: [Autoplay],
 		loop: true,
 		slidesPerView: 1,
 		centeredSlides: true,
 		spaceBetween: 50,
-		onSwiper: swiper => {
-			swiper.slideNext(300);
-		},
 		onSlideChange: swiper => setIndex(swiper.realIndex),
 		breakpoints: {
 			1000: { slidesPerView: 2 },
 			1400: { slidesPerView: 3 }
 		}
 	});
+
+	useEffect(() => {
+		Index === 0 ? setPrevIndex(num.current - 1) : setPrevIndex(Index - 1);
+		Index === num.current - 1 ? setNextIndex(0) : setNextIndex(Index + 1);
+	}, [Index]);
 
 	return (
 		<figure className='Visual'>
@@ -42,7 +49,7 @@ export default function Visual() {
 			<Swiper {...swiperOpt.current}>
 				{isSuccess &&
 					data.map((el, idx) => {
-						if (idx >= 5) return null;
+						if (idx >= num.current) return null;
 						return (
 							<SwiperSlide key={el.id}>
 								<div className='pic'>
@@ -57,6 +64,18 @@ export default function Visual() {
 						);
 					})}
 			</Swiper>
+			<nav className='preview'>
+				{isSuccess && (
+					<>
+						<p className='prevBox'>
+							<img src={data[PrevIndex].snippet.thumbnails.default.url} alt={data[PrevIndex].snippet.title} />
+						</p>
+						<p className='nextBox'>
+							<img src={data[NextIndex].snippet.thumbnails.default.url} alt={data[NextIndex].snippet.title} />
+						</p>
+					</>
+				)}
+			</nav>
 		</figure>
 	);
 }
