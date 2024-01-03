cc4898a9e5564d165d2203a9a9288ad50ea93c90
seohyon-cho <flanique@naver.com>
2024-01-03 12:43:03 +0900

    swiper 컴포넌트 활성화 이벤트를 통해서 인스턴스 활용 (slidesPerView, onSwiper, centeredSlides, spaceBetween 등 여러 옵션 활용)

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,12 +1,26 @@
 import { useSelector } from 'react-redux';
 import './Visual.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useRef } from 'react';
+import { Autoplay } from 'swiper';
+import 'swiper/css';
 
 export default function Visual() {
 	const Vids = useSelector(store => store.youtube.data);
+	const swiperOpt = useRef({
+		modules: [Autoplay],
+		autoplay: { delay: 4000, disableOnInteraction: true },
+		spaceBetween: 50,
+		loop: true,
+		slidesPerView: 3,
+		centeredSlides: true,
+		onSwiper: swiper => {
+			swiper.slideNext(300);
+		}
+	});
 	return (
 		<figure className='Visual'>
-			<Swiper>
+			<Swiper {...swiperOpt.current}>
 				{Vids.map((data, idx) => {
 					if (idx >= 5) return null;
 					return (
