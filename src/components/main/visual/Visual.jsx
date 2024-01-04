e0934cbeb205b0d82cb38934be526c426cda856c
seohyon-cho <flanique@naver.com>
2024-01-04 09:43:42 +0900

    swiper 좌우 미리보기 버튼 생성 및 이벤트 연결

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -7,10 +7,14 @@ import { useRef, useState, useEffect } from 'react';
 
 export default function Visual() {
 	const num = useRef(5);
+	const swiperRef = useRef(null);
 	const { isSuccess, data } = useYoutubeQuery();
+	// loop 값이 true일 시 초기 Index값을 0, 1을 주면 안 됨.
+	// onSwipe 이벤트 발생 시 자동적으로 realIndex값이 기존 Index값에 1을 뺀값으로 적용되므로
+	// useEffect에 의해서 prevIndex값이 0혹은 마지막 순번으로 변경되므로 기존 realIndex값과 중첩되서 버그발생
 	const [Index, setIndex] = useState(1);
-	const [PrevIndex, setPrevIndex] = useState(0);
-	const [NextIndex, setNextIndex] = useState(2);
+	const [PrevIndex, setPrevIndex] = useState(2);
+	const [NextIndex, setNextIndex] = useState(3);
 
 	const swiperOpt = useRef({
 		modules: [Autoplay],
@@ -19,11 +23,20 @@ export default function Visual() {
 		centeredSlides: true,
 		spaceBetween: 50,
 		onSlideChange: swiper => setIndex(swiper.realIndex),
+		onSwiper: swiper => (swiperRef.current = swiper),
 		breakpoints: {
 			1000: { slidesPerView: 2 },
 			1400: { slidesPerView: 3 }
 		}
 	});
+
+	const trimTitle = title => {
+		let resultTit = '';
+		if (title.includes('(')) resultTit = title.split('(')[0];
+		else if (title.includes('[')) resultTit = title.split('[')[0];
+		else resultTit = title;
+		return resultTit;
+	};
 
 	useEffect(() => {
 		Index === 0 ? setPrevIndex(num.current - 1) : setPrevIndex(Index - 1);
@@ -40,7 +53,7 @@ export default function Visual() {
 
 							return (
 								<li key={el.id} className={idx === Index ? 'on' : ''}>
-									<h3>{el.snippet.title}</h3>
+									<h3>{trimTitle(el.snippet.title)}</h3>
 								</li>
 							);
 						})}
@@ -67,10 +80,10 @@ export default function Visual() {
 			<nav className='preview'>
 				{isSuccess && (
 					<>
-						<p className='prevBox'>
+						<p className='prevBox' onClick={() => swiperRef.current.slidePrev(400)}>
 							<img src={data[PrevIndex].snippet.thumbnails.default.url} alt={data[PrevIndex].snippet.title} />
 						</p>
-						<p className='nextBox'>
+						<p className='nextBox' onClick={() => swiperRef.current.slideNext(400)}>
 							<img src={data[NextIndex].snippet.thumbnails.default.url} alt={data[NextIndex].snippet.title} />
 						</p>
 					</>
