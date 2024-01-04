61ddad4b7d7e3feacd233980ef633ae2cab67252
seohyon-cho <flanique@naver.com>
2024-01-04 11:07:13 +0900

    세로 Btns (섹션간 이동 기능) 기본 UI 스타일링

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,5 +1,18 @@
+import { useRef, useState } from 'react';
 import './Btns.scss';
 
 export default function Btns() {
-	return <ul className='Btns'>Btns</ul>;
+	const [Index, setIndex] = useState(0);
+
+	const num = useRef(4);
+
+	return (
+		<ul className='Btns'>
+			{Array(num.current)
+				.fill()
+				.map((_, idx) => {
+					return <li key={idx} className={idx === Index ? 'on' : ''}></li>;
+				})}
+		</ul>
+	);
 }
