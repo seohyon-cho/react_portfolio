8ae4a5918a864f3c619b5805a12562bf90180f46
seohyon-cho <flanique@naver.com>
2024-01-08 08:47:12 +0900

    state에 따른 화면 재렌더링 (Banner 기본 형태 생성)

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState, useRef } from 'react';
+import { useScroll } from '../../../hooks/useScroll';
 import './Banner.scss';
 
 export default function Banner() {
-	return <section className='Banner myScroll'>Banner</section>;
+	console.log('Banner');
+	const refBanner = useRef(null);
+	const [Frame, setFrame] = useState(null);
+	const [Scrolled, setScrolled] = useState(0);
+	console.log(Scrolled);
+
+	const { getCurrentScroll } = useScroll(Frame);
+
+	useEffect(() => {
+		setFrame(refBanner.current?.closest('.wrap'));
+		Frame?.addEventListener('scroll', () => {
+			const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
+			scroll >= 0 && setScrolled(scroll);
+		});
+	}, [Frame, getCurrentScroll]);
+
+	useScroll();
+	return (
+		<section className='Banner myScroll' ref={refBanner}>
+			<div
+				className='box'
+				style={{
+					transform: `rotate(${Scrolled / 2}deg) scale(${1 + Scrolled / 400})`,
+					opacity: 1 - Scrolled / 400
+				}}></div>
+		</section>
+	);
 }
