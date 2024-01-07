f0ee673f223beb3c32450cbe4920b08cb38ef529
seohyon-cho <flanique@naver.com>
2024-01-08 08:10:38 +0900

    scroll되는 frame 요소와 scroll 값 반환함수 추가

--- src/components/common/layout2/Layout2.jsx
+++ src/components/common/layout2/Layout2.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useRef } from 'react';
 import './Layout2.scss';
 import { useSplitText } from '../../../hooks/useText';
-import { useScroll } from '../../../hooks/useScroll';
+import { useScroll, getCurrentScroll, scrollFrame } from '../../../hooks/useScroll';
 
 export default function Layout2({ children, title }) {
 	const refFrame = useRef(null);
 	const refTitle = useRef(null);
 	const splitText = useSplitText();
-	const { scrollTo } = useScroll();
+	const { scrollTo, getCurrentScroll, scrollFrame } = useScroll();
 
 	useEffect(() => {
 		scrollTo(0);
@@ -15,7 +15,10 @@ export default function Layout2({ children, title }) {
 		setTimeout(() => {
 			refFrame.current?.classList.add('on');
 		}, 300);
-	}, [splitText, title, scrollTo]);
+		scrollFrame.current.addEventListener('scroll', () => {
+			console.log(getCurrentScroll());
+		});
+	}, [splitText, title, scrollTo, scrollFrame, getCurrentScroll]);
 
 	return (
 		<main ref={refFrame} className={`Layout ${title}`}>
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -3,18 +3,24 @@ import Anime from '../asset/anime';
 
 export function useScroll(frame = '.wrap') {
 	// 선택자로 스크롤을 제어해야 하는 root 컴포넌트의 클래스명을 받아 DOM요소를 참조객체에 연결
-	const frameRef = useRef(null);
+	const scrollFrame = useRef(null);
 
 	// targetPos 위치값으로 스크롤을 이동하는 함수 정의
 	const scrollTo = targetPos => {
-		new Anime(frameRef.current, { scroll: targetPos });
+		new Anime(scrollFrame.current, { scroll: targetPos });
+	};
+
+	//실시간 scroll값 반환 메서드
+	const getCurrentScroll = () => {
+		const scroll = scrollFrame.current.scrollTop;
+		return scroll;
 	};
 
 	// 컴포넌트 마운트 시, 빈 frameRef에 선택자 담음.
 	useEffect(() => {
-		frameRef.current = document.querySelector(frame);
+		scrollFrame.current = document.querySelector(frame);
 	}, [frame]);
 
 	// scrollTo 함수를 비구조화할당으로 뽑아내기 위해 객체로 묶어서 반환.
-	return { scrollTo };
+	return { scrollTo, getCurrentScroll, scrollFrame };
 }
