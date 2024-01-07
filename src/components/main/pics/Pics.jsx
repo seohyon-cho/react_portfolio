3aafac07dd81834d0d4c8c2035478380e8f1cbf3
seohyon-cho <flanique@naver.com>
2024-01-08 08:37:31 +0900

    Pics 박스에 Scrolled 값 연동 + scrolled값 연동방식 주석 설명

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -1,18 +1,37 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useScroll } from '../../../hooks/useScroll';
 import './Pics.scss';
 
 export default function Pics() {
-	const { scrollFrame, getCurrentScroll } = useScroll();
+	console.log('Pics');
+	const [Frame, setFrame] = useState(null);
+	const thisEl = useRef(null);
+	const boxEl = useRef(null);
+	const { getCurrentScroll } = useScroll(Frame);
+
+	//hook으로부터 현재 보정된 scroll값을 반환받아서
+	//해당 값을 참조객체의 style값으로 연동
+	//scroll값이 바뀔때마다 불필요하게 재랜더링하지 않으면서 스크롤값 스타일에 연동
+	const handleScroll = useCallback(() => {
+		const scroll = getCurrentScroll(thisEl.current);
+		scroll >= 0 && (boxEl.current.style.transform = `translateX(${scroll}px)`);
+	}, [getCurrentScroll]);
+
+	//컴포넌트 마운트시 wrap요소를 Frame State에담음
+	//State에 담겨있는 wrap요소는 최신 scrollTop을 가지고 있는 동적인 상태
+	useEffect(() => {
+		setFrame(thisEl.current?.closest('.wrap'));
+	}, []);
 
 	useEffect(() => {
-		scrollFrame?.addEventListener('scroll', () => {
-			console.log(getCurrentScroll());
-		});
-	}, [scrollFrame, getCurrentScroll]);
+		//Frame에 scroll이벤트 연결
+		Frame?.addEventListener('scroll', handleScroll);
+		return () => Frame?.removeEventListener('scroll', handleScroll);
+	}, [Frame, handleScroll]);
+
 	return (
-		<section className='Pics myScroll'>
-			<div className='box'></div>
+		<section className='Pics myScroll' ref={thisEl}>
+			<div className='box' ref={boxEl}></div>
 		</section>
 	);
 }
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,26 +1,19 @@
-import { useEffect, useRef } from 'react';
 import Anime from '../asset/anime';
 
-export function useScroll(frame = '.wrap') {
-	// 선택자로 스크롤을 제어해야 하는 root 컴포넌트의 클래스명을 받아 DOM요소를 참조객체에 연결
-	const scrollFrame = useRef(null);
-
-	// targetPos 위치값으로 스크롤을 이동하는 함수 정의
+export function useScroll(scrollFrame) {
+	//기존에 scrollTop값을 제어하는 wrap요소를 참조객체에 담아서 반환하는것이 문제
+	//이유: wrap.scrollTop의 변경되는 값을 계속 활용해되는데
+	//참조객체에 담으면 담는 순간의 참조객체값이 고정되는 문제 발생
+	//해결방법: wrap요소를 호출 부모컴포넌트에서 State에 담도록 처리
 	const scrollTo = targetPos => {
 		new Anime(scrollFrame.current, { scroll: targetPos });
 	};
 
-	//실시간 scroll값 반환 메서드
-	const getCurrentScroll = () => {
-		const scroll = scrollFrame.current.scrollTop;
-		return scroll;
+	const getCurrentScroll = selfEl => {
+		const scroll = scrollFrame.scrollTop;
+		const modifiedScroll = scroll - selfEl?.offsetTop;
+		return modifiedScroll;
 	};
 
-	// 컴포넌트 마운트 시, 빈 frameRef에 선택자 담음.
-	useEffect(() => {
-		scrollFrame.current = document.querySelector(frame);
-	}, [frame]);
-
-	// scrollTo 함수를 비구조화할당으로 뽑아내기 위해 객체로 묶어서 반환.
-	return { scrollTo, getCurrentScroll, scrollFrame: scrollFrame.current };
+	return { scrollTo, getCurrentScroll };
 }
