2b410d1aa20b866b997ae9c421be6c677225ec5e
seohyon-cho <flanique@naver.com>
2024-01-08 10:22:27 +0900

    각각의 컴포넌트에서 일일이 참조객체를 만들지 않아도 되게끔, useScroll 커스텀 훅 안쪽에서 아예 자체적으로 빈 참조객체를 만들어서 부모 컴포넌트에 return하여 부모 컴포넌트에서는 가져온 빈 참조객체를 바로 활용할 수 있게끔 로직 변경

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -3,13 +3,13 @@ import { useScroll } from '../../../hooks/useScroll';
 import './Banner.scss';
 
 export default function Banner() {
-	const refBanner = useRef(null);
 	const boxEl = useRef(null);
-	const { getCurrentScroll, Frame } = useScroll();
+	// 순서 (3) 커스텀 훅 호출 시, useScroll이 제공하고 있는 (이전에 생성해둔) 빈 참조객체를 가져옴.
+	const { getCurrentScroll, Frame, refEl } = useScroll();
 	// 계속 해서 다른 값을 반환하는 getCurrentScroll을 useRef에 담아버리면 스크롤값의 맨 처음 초기값 0만이 정적으로 담기게 되면서 스크롤 작동이 정상적으로 안 됨.
 	// useCallback에 담음으로써 메모이제이션 처리를 하되, getCurrentScroll의 값 (스크롤값)이 변화할 때마다 메모이제이션을 잠시 풀고 값 변경.
 	const handleScroll = useCallback(() => {
-		const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
+		const scroll = getCurrentScroll(-window.innerHeight / 2);
 		if (scroll >= 0) {
 			boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
 			boxEl.current.style.opacity = 1 - scroll / 400;
@@ -23,7 +23,8 @@ export default function Banner() {
 
 	useScroll();
 	return (
-		<section className='Banner myScroll' ref={refBanner}>
+		// 순서 (4) 원하는 요소에 빈 참조객체 연결
+		<section className='Banner myScroll' ref={refEl}>
 			<div className='box' ref={boxEl}></div>
 		</section>
 	);
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,17 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Anime from '../asset/anime';
 
 //useScroll훅을 처음 초기화할때 무조건 인수로 state에 담겨있는 ScrollFrame요소를 전달 (중요)
 export function useScroll() {
+	// 순서 (1) 커스텀 훅 안쪽에 자체적으로 빈 참조객체 생성.
+	const refEl = useRef(null);
 	const [Frame, setFrame] = useState(null);
 	const scrollTo = targetPos => {
 		Frame && new Anime(Frame, { scroll: targetPos });
 	};
 
 	//getCurrentScroll(호출하는 부모프레임요소, 기준점 보정값)
-	const getCurrentScroll = (selfEl, baseLine = 0) => {
+	const getCurrentScroll = (baseLine = 0) => {
 		const scroll = Frame.scrollTop - baseLine;
-		const modifiedScroll = scroll - selfEl?.offsetTop;
+		// 순서 (5) 부모 컴포넌트에서 참조 객체에 연결된 값을 커스텀 훅에서 내부적으로 활용함.
+		const modifiedScroll = scroll - refEl.current?.offsetTop;
 		return modifiedScroll;
 	};
 
@@ -20,5 +23,6 @@ export function useScroll() {
 		setFrame(document.querySelector('.wrap'));
 	}, []);
 
-	return { scrollTo, getCurrentScroll, Frame };
+	// 순서 (2) 부모 컴포넌트에서 해당 참조객체를 활용할 수 있도록 return으로 반환처리하여 전달.
+	return { scrollTo, getCurrentScroll, Frame, refEl };
 }
