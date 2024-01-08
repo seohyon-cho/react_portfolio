97e800b7242d32f8a0a27f794477f6dbf6266b34
seohyon-cho <flanique@naver.com>
2024-01-08 10:12:01 +0900

    scroll 제어를 위한 .wrap을 담을 Frame state를, 호출하는 부모 컴포넌트 쪽이 아닌 useScroll 커스텀 훅에서 자체적으로 담아서 부모 컴포넌트로 반환 처리해주는 로직

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -3,11 +3,9 @@ import { useScroll } from '../../../hooks/useScroll';
 import './Banner.scss';
 
 export default function Banner() {
-	const [Frame, setFrame] = useState(null);
 	const refBanner = useRef(null);
-
 	const boxEl = useRef(null);
-	const { getCurrentScroll } = useScroll(Frame);
+	const { getCurrentScroll, Frame } = useScroll();
 	// 계속 해서 다른 값을 반환하는 getCurrentScroll을 useRef에 담아버리면 스크롤값의 맨 처음 초기값 0만이 정적으로 담기게 되면서 스크롤 작동이 정상적으로 안 됨.
 	// useCallback에 담음으로써 메모이제이션 처리를 하되, getCurrentScroll의 값 (스크롤값)이 변화할 때마다 메모이제이션을 잠시 풀고 값 변경.
 	const handleScroll = useCallback(() => {
@@ -17,10 +15,6 @@ export default function Banner() {
 			boxEl.current.style.opacity = 1 - scroll / 400;
 		}
 	}, [getCurrentScroll]);
-
-	useEffect(() => {
-		setFrame(refBanner.current?.closest('.wrap'));
-	}, []);
 
 	useEffect(() => {
 		Frame?.addEventListener('scroll', handleScroll);
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,16 +1,24 @@
+import { useState, useEffect } from 'react';
 import Anime from '../asset/anime';
 
 //useScroll훅을 처음 초기화할때 무조건 인수로 state에 담겨있는 ScrollFrame요소를 전달 (중요)
 export function useScroll(scrollFrame) {
+	const [Frame, setFrame] = useState(null);
 	const scrollTo = targetPos => {
-		scrollFrame && new Anime(scrollFrame, { scroll: targetPos });
+		Frame && new Anime(Frame, { scroll: targetPos });
 	};
 
 	//getCurrentScroll(호출하는 부모프레임요소, 기준점 보정값)
 	const getCurrentScroll = (selfEl, baseLine = 0) => {
-		const scroll = scrollFrame?.scrollTop - baseLine;
+		const scroll = Frame.scrollTop - baseLine;
 		const modifiedScroll = scroll - selfEl?.offsetTop;
 		return modifiedScroll;
 	};
-	return { scrollTo, getCurrentScroll };
+
+	useEffect(() => {
+		// .wrap은 리액트 기반의 최상위 루트 요소이므로, 사라지거나 나타나거나 등 변경될 요소가 아니므로 querySelector로 가져와도 됨.
+		setFrame(document.querySelector('.wrap'));
+	}, []);
+
+	return { scrollTo, getCurrentScroll, Frame };
 }
