3cfe43dee4e7c89b6a3ed8f07ea5a8374cabd38d
seohyon-cho <flanique@naver.com>
2024-01-08 10:03:04 +0900

    banner 컴포넌트 내 스크롤 관련 구문 구조 개선 (핸들러 함수 생성하여 그 안쪽에서 scroll값 연동하여 활용하도록 처리)

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,33 +1,36 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import { useScroll } from '../../../hooks/useScroll';
 import './Banner.scss';
 
 export default function Banner() {
-	console.log('Banner');
-	const refBanner = useRef(null);
 	const [Frame, setFrame] = useState(null);
-	const [Scrolled, setScrolled] = useState(0);
-	console.log(Scrolled);
+	const refBanner = useRef(null);
 
+	const boxEl = useRef(null);
 	const { getCurrentScroll } = useScroll(Frame);
+	// 계속 해서 다른 값을 반환하는 getCurrentScroll을 useRef에 담아버리면 스크롤값의 맨 처음 초기값 0만이 정적으로 담기게 되면서 스크롤 작동이 정상적으로 안 됨.
+	// useCallback에 담음으로써 메모이제이션 처리를 하되, getCurrentScroll의 값 (스크롤값)이 변화할 때마다 메모이제이션을 잠시 풀고 값 변경.
+	const handleScroll = useCallback(() => {
+		const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
+		if (scroll >= 0) {
+			boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
+			boxEl.current.style.opacity = 1 - scroll / 400;
+		}
+	}, [getCurrentScroll]);
 
 	useEffect(() => {
 		setFrame(refBanner.current?.closest('.wrap'));
-		Frame?.addEventListener('scroll', () => {
-			const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
-			scroll >= 0 && setScrolled(scroll);
-		});
-	}, [Frame, getCurrentScroll]);
+	}, []);
+
+	useEffect(() => {
+		Frame?.addEventListener('scroll', handleScroll);
+		return () => Frame?.removeEventListener('scroll', handleScroll);
+	}, [Frame, handleScroll]);
 
 	useScroll();
 	return (
 		<section className='Banner myScroll' ref={refBanner}>
-			<div
-				className='box'
-				style={{
-					transform: `rotate(${Scrolled / 2}deg) scale(${1 + Scrolled / 400})`,
-					opacity: 1 - Scrolled / 400
-				}}></div>
+			<div className='box' ref={boxEl}></div>
 		</section>
 	);
 }
