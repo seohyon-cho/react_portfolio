913f208349480008594157d3bf13c2131c330af0
seohyon-cho <flanique@naver.com>
2024-01-08 12:24:41 +0900

    다크모드 적용 안 되는 현상 수정 (Darkmode.jsx 구조 수정)

--- src/components/common/darkMode/DarkMode.jsx
+++ src/components/common/darkMode/DarkMode.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useCookie } from '../../../hooks/useCookie';
 import { useGlobalData } from '../../../hooks/useGlobalData';
 import './DarkMode.scss';
@@ -6,16 +7,16 @@ export default function DarkMode() {
 	const { Mode, setMode } = useGlobalData();
 	const { setCookie, isCookie } = useCookie();
 
-	if (isCookie('dark')) {
-		setMode(document.cookie.split('dark=')[1].split(';')[0]);
-	}
+	useEffect(() => {
+		if (isCookie('mode')) setMode(document.cookie.split('mode=')[1].split(';')[0]);
+	}, []);
 	const changeMode = () => {
-		setMode(Mode === 'dark' ? 'dark' : 'light');
-		setCookie('dark', Mode, 20);
+		setMode(Mode === 'light' ? 'dark' : 'light');
+		setCookie('mode', Mode === 'light' ? 'dark' : 'light', 60 * 60 * 24);
 	};
 
 	return (
-		<div className={`DarkMode ${Mode === 'dark' ? 'dark' : 'light'}`} onClick={changeMode}>
+		<div className={`DarkMode ${Mode === 'light' ? 'light' : 'dark'}`} onClick={changeMode}>
 			<div className='ball'></div>
 		</div>
 	);
