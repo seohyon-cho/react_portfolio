de8902ab0ff6dd8d47028f339787101e395f232b
seohyon-cho <flanique@naver.com>
2024-01-12 10:13:21 +0900

    App.jsx에서의 이슈사항 및 개발 흐름 주석 정리

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ export default function App() {
 	useSelector(store => console.log(store));
 	const path = useRef(process.env.PUBLIC_URL);
 
-	// department 관련 패칭
 	const fetchDepartment = useCallback(async () => {
 		const data = await fetch(`${path.current}/DB/department.json`);
 		const json = await data.json();
@@ -77,10 +76,23 @@ export default function App() {
 }
 
 /*
-	1. reducer, store, app으로의 데이터 전달 흐름 이해
-	2. reducer가 하는 역할
-	3. action 객체가 필요한 이유 
-	4. 컴포넌트에서 데이터 호출 및 데이터 변경 요청을 위한 useSelector, useDispatch
-	5. App에서 fetching 후, action 객체를 통해 dispatch로 reducer에 데이터 변경 요청 흐름.
-	6. action의 type을 따로 객체 형태로 만들어 관리하는 이유 
+	[ 해당 페이지 (App.jsx) 에서의 개발 흐름 ]
+
+	- 루트 컴포넌트이기 때문에, 모든 비동기 데이터를 호출한 뒤, 액션 객체를 생성해서 dispatch로 Reducer에 전달한 후, store(전역) 에 저장. 
+
+	- 메인 페이지 및 서브 페이지용 컴포넌트를 Router 설정 
+			: 실제 실무에서, 아직 레거시코드 (옛날코드) 를 많이 쓰고 있기 때문에, 일부러 react-router-dom 을 5버전(구버전)을 사용해봤고, 
+			같은 원리로 프로젝트에서 redux도 활용해보고자 일부러 React17버전으로 다운그레이드해서 작업했음. 
+
+	- 미디어쿼리로 반응형 처리를 하지 않는 이유 : module.scss는 아니지만, 컴포넌트의 효율적인 유지보수를 위해서 폴더별로 jsx, scss 파일을 그룹화하다보니 
+	일반 미디어쿼리로 반응형 처리를 하기에는 반복 작업과 css 변수 활용에 어려움이 있어, useMedia 라는 커스텀 훅을 직접 만들어서, 스크립트로 반응형 스타일링이 되도록 처리했음. 
+
+
+
+
+	[ 해당 프로젝트 진행하면서 발생한 이슈 사항 ]
+
+	(1) 처음에 비동기 데이터를 여러 컴포넌트에서 재활용하기 위해서 redux를 사용했는데, flickr 처럼 검색이나 사용자 이벤트에 따라 빈번하게 비동기 데이터를 다시 담아야 하는 경우에는 redux로 처리하기가 힘들어 적합하지 않았다고 느꼈음. 
+	
+	(1)의 해결 절차 : flickr만 redux에서 제외하고 고민하던 중, redux의 비동기데이터의 전역 State 처리를 위한 redux-saga가 있다는 걸 알게 됨. 그래서 branch를 따로 분리해서 redux-saga 버전으로 작업을 진행하기 시작함. 
 */
