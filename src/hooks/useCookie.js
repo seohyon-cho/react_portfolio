d52e5430aca91edb28b8c25f8d25d9de908fff50
seohyon-cho <flanique@naver.com>
2024-01-15 09:45:58 +0900

    useCookie 훅 개념 정리

--- src/hooks/useCookie.js
+++ src/hooks/useCookie.js
@@ -16,3 +16,11 @@ export function useCookie() {
 
 	return { setCookie, isCookie, viewCookie };
 }
+
+/*
+	useCookie()훅 제작 배경
+	: 복수개의 컴포넌트에서 쿠키를 생성하거나 사용해야될때 동일한 쿠키생성로직 재활용을 위해 제작
+	setCookie(쿠키이름, 쿠키값, 만료시간(초단위)) : 특정 쿠키를 원하는 시간만큼 생성 (쿠키삭제기능 가능)
+	isCookie(쿠키이름) : 현재 브라우저에 해당 이름의 쿠키가 있는지 반환 (true있음, false없음)
+	viewCookie() : 현재 브라우저 등록된 모든 쿠키값 확인
+*/
