e49a49fbbb154486b34b1c0ba3fc4d5c7763ce25
seohyon-cho <flanique@naver.com>
2024-01-15 10:01:30 +0900

    useGlobalData 훅 개념 정리

--- src/hooks/useGlobalData.js
+++ src/hooks/useGlobalData.js
@@ -16,3 +16,11 @@ export function useGlobalData() {
 	const globalData = useContext(GlobalContext);
 	return globalData;
 }
+
+/*
+	useGlobalData() 훅 사용 배경
+	- 기존 비동기 데이터를 react-query로 관리하면서 굳이 간단한 client side data를 무거운 redux시스템으로 관리할 필요가 없어짐
+	- 기존 react에 내장되어 있는 context API 기반의 createContext, useContext훅을 활용한 커스텀 훅 제작 필요
+	GlobalProvider : 전역으로 관리할 특정 정보값을 담아서 전역인 App에 전달해주는 Wrapping Provider 컴포넌트
+	useGlobalData() : 자식 컴포넌트에서 전역 컨텍스트의 값을 가져오기 위한 커스텀 훅
+*/
